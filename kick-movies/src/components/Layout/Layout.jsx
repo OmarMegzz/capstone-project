978ad501e3ba12915d22edd96602962a066d623c
omar magdy <omarmagdyzz12@gmail.com>
2024-10-02 23:53:06 +0300

    modifi navbar import

--- kick-movies/src/components/Layout/Layout.jsx
+++ kick-movies/src/components/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import NavBar from "../navBar/NavBar.jsx";
+import NavBar from "../NavBar/NavBar.jsx";
 import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
 
